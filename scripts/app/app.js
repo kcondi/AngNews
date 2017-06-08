a8db1c1dcc77d338ee945f44d89c1043f3c8e0ab
Kresimir Condic <v-kresimir.condic@dump.hr>
2017-06-08 20:56:58 +0200

    Added services to reduce code duplication, added custom filter for favorite list.

--- scripts/app/app.js
+++ scripts/app/app.js
@@ -32,28 +32,34 @@ app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 });
 
 app.controller('HomeController',
-    function ($scope, $http, FavoriteService) {
+    function ($scope, $http, GetDefaultArticlesService, FavoriteService) {
         var separateArticles;
         var loadMoreArticlesClicksCounter = 1;
         $scope.allShown = false;
+
+        var defaultArticlesPromise = GetDefaultArticlesService;
+
+        defaultArticlesPromise.then(function(articles) {
+            separateArticles = _.chunk(articles, 6);
+            $scope.articles = separateArticles[0];
+        });
 
         $scope.favoriteArticle = function (articleToFavoriteId) {
             FavoriteService.favoriteArticle(articleToFavoriteId);
         }
 
-        $http.get('defaultNews.json').then(function(articles) {
-            separateArticles = _.chunk(articles.data, 6);
-            $scope.articles = separateArticles[0];
-        });
-
         $scope.search = function () {
             if (!$scope.searchText) {
-                $http.get('defaultNews.json').then(function (articles) {
-                    separateArticles = _.chunk(articles.data, 6);
+                defaultArticlesPromise = GetDefaultArticlesService;
+                defaultArticlesPromise.then(function (articles) {
+                    separateArticles = _.chunk(articles, 6);
                     $scope.articles = separateArticles[0];
                 });
+                $scope.allArticlesAreShown = false;
+                loadMoreArticlesClicksCounter = 1;
             }
-            $scope.articles = _.filter($scope.articles, article => article.title.startsWith($scope.searchText));
+            $scope.articles = _.filter($scope.articles,
+                article => article.title.toLowerCase().startsWith($scope.searchText.toLowerCase()));
         }
 
         $scope.showMore = function () {
@@ -68,34 +74,53 @@ app.controller('HomeController',
     });
 
 app.controller('ArticleDetailsController',
-    function ($scope, $http, $state, $stateParams, FavoriteService) {
+    function ($scope, $http, $state, $stateParams, GetDefaultArticlesService, FavoriteService) {
+
+        var defaultArticlesPromise = GetDefaultArticlesService;
+
+        defaultArticlesPromise.then(function(articles) {
+            $scope.chosenArticle = articles[$stateParams.id - 1];
+        });
 
         $scope.favoriteArticle = function (articleToFavoriteId) {
             FavoriteService.favoriteArticle(articleToFavoriteId);
         }
-
-        $http.get('defaultNews.json').then(function (articles) {
-           $scope.chosenArticle = articles.data[$stateParams.id - 1];
-        });
     });
 
 app.controller('FavoriteListController',
-    function ($scope, $http, localStorageService, GetCurrentFavoritesService) {
+    function ($scope, $http, localStorageService, GetDefaultArticlesService, GetCurrentFavoritesService) {
 
-        $http.get('defaultNews.json').then(function (articles) {
-            $scope.allArticles = articles.data;
+        var defaultArticlesPromise = GetDefaultArticlesService;
+
+        defaultArticlesPromise.then(function(articles) {
+            $scope.favoritedArticles = articles;
         });
-
-        $scope.isArticleFavorited = function (article) {
-            var currentFavoritesIds = GetCurrentFavoritesService.getFavorites();
-            return currentFavoritesIds.indexOf(article.id) > -1;
-        }
-
+        
         $scope.unfavorite = function(articleId) {
             var currentFavoritesIds = GetCurrentFavoritesService.getFavorites();
             currentFavoritesIds.splice(currentFavoritesIds.indexOf(articleId), 1);
             localStorageService.set("favoritedArticles", currentFavoritesIds);
         }
+
+        $scope.search = function () {
+            if (!$scope.searchText) {
+                defaultArticlesPromise = GetDefaultArticlesService;
+                defaultArticlesPromise.then(function (articles) {
+                    $scope.favoritedArticles = articles;
+                });
+            }
+            $scope.favoritedArticles = _.filter($scope.favoritedArticles,
+                article => article.title.toLowerCase()
+                    .startsWith($scope.searchText.toLowerCase()));
+        }
+    });
+
+
+app.service('GetDefaultArticlesService',
+    function($http) {
+        return $http.get('defaultNews.json').then(function(articles) {
+            return articles.data;
+        });
     });
 
 app.service('FavoriteService', function (localStorageService, GetCurrentFavoritesService) {
@@ -114,19 +139,17 @@ app.service('GetCurrentFavoritesService',
         }
     });
 
-/*app.service('SearchArticlesService',
-    function($scope, $http) {
-        $http.get('defaultNews.json').then(function(articles) {
-            return _.filter(articles, article => article.title_.startsWith($scope.searchText));
-        });
-    });*/
-
 app.filter('favoritedArticlesFilter',
-    function(article, localStorageService) {
-        return function () {
+    function(localStorageService) {
+        return function (articleList) {
             var currentFavoritesIds = angular.fromJson
                 (localStorageService.get("favoritedArticles"));
-            console.log(article.id);
-            return currentFavoritesIds.indexOf(article.id) > -1;
+            var filtered = [];
+            angular.forEach(articleList,
+                function(article) {
+                    if (currentFavoritesIds.indexOf(article.id) > -1)
+                        filtered.push(article);
+                });
+            return filtered;
         }
     });
